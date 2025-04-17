d185e9aa852738991081c040a044e394afd2f8fe
Your Name <you@example.com>
2025-04-18 00:42:30 +0530

    Created Login API

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,8 @@ import UserModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
 import { Router } from "express";
+import generatedAccessToken from "../utils/generatedAccessToken.js";
+import genertedRefreshToken from "../utils/generatedRefreshToken.js";
 
 export class UserController{
     
@@ -13,6 +15,8 @@ export class UserController{
 
     configureRoutes(){
         this.router.post("/register",this.registerUser);
+        this.router.post("/verify-email",this.verifyUser);
+        this.router.post("/login",this.loginUser);
     }
     registerUser =async(req,res)=>{
         try{
@@ -34,7 +38,7 @@ export class UserController{
     
             const newUser=new UserModel(payload)
             const save=await newUser.save();
-            const verifyEmailUrl=`${process.env.FRONTEND_URL}/verify-email?codes=${save?._id}`;
+            const verifyEmailUrl=`${process.env.FRONTEND_URL}/verify-email?code=${save?._id}`;
             const verifyEmail= await sendEmail({
                 sendTo:email,
                 subject:"Verify Email",
@@ -52,6 +56,76 @@ export class UserController{
         }
         catch(error){
             return res.status(500).json({message:"cannot register user",error:true,success:false});
+        }
+    }
+
+    verifyUser=async(req,res)=>{
+        try{
+            const {code}=req.query;
+            console.log(code);
+            const user =await UserModel.find({_id:code});
+
+            if(!user){
+                return res.status(404).json({message:"User not found"});
+            }
+
+            const updateUser=await UserModel.updateOne({_id:code},{
+                verify_email:true
+            })
+
+            return res.status(201).json({message:"Verified User"});
+
+        }
+        catch(error){
+            return res.status(500).json({message:"Verification failed",error:true,success:false});
+        }
+    }
+
+    loginUser=async(req,res)=>{
+        try{
+            const {name,email,password}=req.body;
+            if(!name||!email||!password){
+                return res.status(400).json({message:"Please fill required fields"});
+            }
+            const User=await UserModel.findOne({name,email});
+            if(!User){
+                return res.status(404).json({message:"User Not Found!"});
+            }
+
+            if(User.status!=="Active"){
+                return res.status(401).json({message:"This account is not active. Please activate the account"});
+            }
+            
+            const checkPassword=await bcrypt.compare(password,User.password);
+            if(!checkPassword){
+                return res.status(402).json({message:"Wrong Password entered"});
+            }
+
+            const accesstoken = await generatedAccessToken(User._id)
+            const refreshToken = await genertedRefreshToken(User._id)
+
+            const updateUser = await UserModel.findByIdAndUpdate(User?._id,{
+                last_login_date : new Date()
+            })
+
+            const cookiesOption = {
+                httpOnly : true,
+                secure : true,
+                sameSite : "None"
+            }
+            res.cookie('accessToken',accesstoken,cookiesOption)
+            res.cookie('refreshToken',refreshToken,cookiesOption)
+
+            return res.status(202).json({
+                message:"Logged in Successfully",
+                data:{
+                    accesstoken,
+                    refreshToken
+                }
+            });
+        }
+        catch(error){
+            return res.status(500).json({message:`Cannot Login User ${error.message}`,error:true,success:false});
         }
     }
 
